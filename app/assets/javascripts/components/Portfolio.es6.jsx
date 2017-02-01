0a47f8d7382732ceaa7b8b3db692d656ea7901f7
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-31 20:15:16 -0500

    Add auto updating functionality.

--- app/assets/javascripts/components/Portfolio.es6.jsx
+++ app/assets/javascripts/components/Portfolio.es6.jsx
@@ -12,19 +12,27 @@ class Portfolio extends React.Component {
     }
     this.handleClick = this.handleClick.bind(this)
     this.getPositions = this.getPositions.bind(this)
+    // this.instantLoad = this.instantLoad.bind(this)
   }
 
   getPositions(userId,id) {
-    $.ajax({
-      url: 'portfolios/' + id + '/positions'
-    }).done((response) => {
-      this.setState({positions: [].concat.apply([], response.query.results.quote)})
-    })
+      $.ajax({
+        url: 'portfolios/' + id + '/positions'
+      }).done((response) => {
+        this.setState({positions: [].concat.apply([], response.query.results.quote)})
+      });
+  }
+
+  instantLoad(userId,id) {
+    this.getPositions(userId, id)
+    var timer = setInterval(() => {
+      this.getPositions(userId, id)
+    }, 5000)
   }
 
   handleClick(event) {
     event.preventDefault()
-    this.getPositions(this.props.userId,this.props.portfolioId)
+    this.instantLoad(this.props.userId,this.props.portfolioId)
     let status = this.state.details
     this.setState({details: !status})
   }
